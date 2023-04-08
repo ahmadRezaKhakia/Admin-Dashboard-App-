ed61a8b8c5766799766e73953e0a2fdbebf71294
AhmadReza Khakia <khakia7961@gmail.com>
2023-04-08 11:03:42 +0430

    fix bug and create dark mode

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {ColorModeContext , useMode} from './Theme'
+import { CssBaseline,ThemeProvider } from '@mui/material';
+import TopBar from './scenes/global/TopBar';
+
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <main className='content'>
+            <TopBar/>
+          </main>
+    </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+      
+  )
+}
+
+export default App
